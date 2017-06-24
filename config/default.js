e19a1e66124d79b20d91d307b081092615419ed3
yucaiyolanda <caiyuyolanda@163.com>
2017-06-24 15:18:00 +0800

    modify

---
+++ config/default.js
@@ -0,0 +1,10 @@
+module.exports = {
+  port: 3000,
+  session: {
+    secret: 'myblog',
+    key: 'myblog',
+    maxAge: 2592000000
+  },
+ //mongodb: 'mongodb://localhost:27017/myblog'
+ mongodb: 'mongodb://yucai:yucai0318242826@ds147821.mlab.com:47821/caiyuyolanda_blog'
+};
